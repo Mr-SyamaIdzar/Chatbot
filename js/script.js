2111747ed0c29c2c09c2b5b69ae34f823ff649f0
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-03-03 19:47:51 +0700

    Handling user message

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
+const chatsContainer = document.querySelector(".chats-container");
 const promptFrom = document.querySelector(".prompt-form");
 const promptInput = promptFrom.querySelector(".prompt-input");
 
 let userMessage = "";
+
+// Function to create message elements
+const createMsgElement = (content, className) => {
+  const div = document.createElement("div");
+  div.classList.add("message", className);
+  div.innerHTML = content;
+  return div;
+};
 
 // Handle the form submission
 const handleFormSubmit = (e) => {
@@ -11,7 +20,12 @@ const handleFormSubmit = (e) => {
     return;
   }
 
-  console.log(userMessage);
+  // Generate user message HTML and add in the chats container
+  const userMsgHTML = `<p class="message-text"></p>`;
+  const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
+
+  userMsgDiv.querySelector(".message-text").textContent = userMessage
+  chatsContainer.appendChild(userMsgDiv)
 };
 
 promptFrom.addEventListener("submit", handleFormSubmit);
