466c8fe7559e50cfcf9e4b7bd3ea913152f7be75
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-03-03 19:58:17 +0700

    Managing bot responses

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const promptInput = promptFrom.querySelector(".prompt-input");
 let userMessage = "";
 
 // Function to create message elements
-const createMsgElement = (content, className) => {
+const createMsgElement = (content, ...classes) => {
   const div = document.createElement("div");
-  div.classList.add("message", className);
+  div.classList.add("message", ...classes);
   div.innerHTML = content;
   return div;
 };
@@ -20,12 +20,24 @@ const handleFormSubmit = (e) => {
     return;
   }
 
+  promptInput.value = "";
+
   // Generate user message HTML and add in the chats container
   const userMsgHTML = `<p class="message-text"></p>`;
   const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
 
-  userMsgDiv.querySelector(".message-text").textContent = userMessage
-  chatsContainer.appendChild(userMsgDiv)
+  userMsgDiv.querySelector(".message-text").textContent = userMessage;
+  chatsContainer.appendChild(userMsgDiv);
+
+  setTimeout(() => {
+    // Generate bot message HTML and add in the chats container after 600ms
+    const botMsgHTML = `
+    <img src="assets/img/gemini-chatbot-logo.svg" alt="Gemini" class="avatar">
+    <p class="message-text">Just a sec...</p>
+    `;
+    const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
+    chatsContainer.appendChild(botMsgDiv);
+  }, 600);
 };
 
 promptFrom.addEventListener("submit", handleFormSubmit);
